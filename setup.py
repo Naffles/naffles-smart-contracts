a7ce43fc1be3159dc32c309747ce0787f8d7f9e1
0xJax <jaxdoubleu@protonmail.com>
2022-12-12 11:42:16 +0100

    changed requirements'

---
+++ setup.py
@@ -0,0 +1,14 @@
+import os
+
+from setuptools import setup
+
+requires = [
+    'brownie~=1.19.2'
+]
+
+setup(
+    name='Naffles',
+    version="0.0.1",
+    author='Naffles',
+    install_requires=requires,
+)
