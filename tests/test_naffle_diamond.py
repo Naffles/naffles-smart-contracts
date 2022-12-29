3d48cd9f0fc30eed6da0f467183ac10c7aa1b5c2
0xJax <jaxdoubleu@protonmail.com>
2022-12-29 18:30:13 +0100

    added the test

--- tests/test_naffle_diamond.py
+++ tests/test_naffle_diamond.py
@@ -5,27 +5,37 @@ from brownie import Contract, TestValueStorage, TestValueFacet
 
 NULL_ADDRESS = "0x0000000000000000000000000000000000000000"
 
-def test_faucet_deployment(
-    from_admin,
-    deployed_naffle_diamond,
-    deployed_test_facet,
-    deployed_test_storage
-):
-    cut = [
+
+def _add_facets(diamond, facet, from_admin):
+   cut = [
         [
-            deployed_test_facet.address,
+            facet.address,
             FacetCutAction.ADD.value,
             getSelectors(TestValueFacet)
         ]
-    ]
-        
-    start_facet_number = len(deployed_naffle_diamond.facets())
-
-    deployed_naffle_diamond.diamondCut(
+    ] 
+   diamond.diamondCut(
         cut, 
         NULL_ADDRESS,
         b'',
         from_admin
     )
+
+def test_faucet_deployment(
+    from_admin,
+    deployed_naffle_diamond,
+    deployed_test_facet,
+):
+    start_facet_number = len(deployed_naffle_diamond.facets())
+
+    _add_facets(deployed_naffle_diamond, deployed_test_facet, from_admin)
     
     assert len(deployed_naffle_diamond.facets()) == start_facet_number + 1
+
+
+def test_set_and_get_value(from_admin, deployed_naffle_diamond, deployed_test_facet):
+    _add_facets(deployed_naffle_diamond, deployed_test_facet, from_admin)
+
+    deployed_test_facet.setValue("value", from_admin)
+    assert deployed_test_facet.getValue() == "value"
+
