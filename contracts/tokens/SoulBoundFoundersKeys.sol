374b75f0d00fc61d56343e83b1169977fa1d85f1
0xJax <jaxdoubleu@protonmail.com>
2023-01-03 13:03:19 +0100

    succefull compile

--- contracts/tokens/SoulBoundFoundersKeys.sol
+++ contracts/tokens/SoulBoundFoundersKeys.sol
@@ -8,16 +8,16 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@ERC721A/contracts/interfaces/IERC721A.sol";
 
 error SouldBound();
-error AlreadyMinted(uin256 tokenId);
+error AlreadyMinted(uint256 tokenId);
 error NotOwnerOfToken(uint256 tokenId);
 
-contract SoulBoundFoundersKey ERC721, Ownable, AccessControl {
+contract SoulBoundFoundersKey is ERC721, Ownable, AccessControl {
     using Address for address;
     address public foundersKeysAddress;
     bytes32 public constant STAKING_CONTRACT_ROLE = keccak256("STAKING_CONTRACT");
 
-    Event Minted(uint256 tokenId, address owner);
-    Event Burned(uint256 tokenId);
+    event Minted(uint256 tokenId, address owner);
+    event Burned(uint256 tokenId);
     
     constructor(address _stakingAddress, address _foundersKeysAddress) ERC721("Souldbound Naffles Founders Keys", "SBNFLS") {
       foundersKeysAddress = _foundersKeysAddress;
@@ -27,27 +27,27 @@ contract SoulBoundFoundersKey ERC721, Ownable, AccessControl {
     function safeMint(address _to, uint256 _tokenId) public onlyRole(STAKING_CONTRACT_ROLE) {
       if (IERC721A(foundersKeysAddress).ownerOf(_tokenId) != _to) {
         revert NotOwnerOfToken(_tokenId);
-      };
+      }
       _safeMint(_to, _tokenId);
 
       emit Minted(_tokenId, _to);
     }
 
-    function burn(uint256 _tokenId) external onlyRole(STAKING_CONTRACT) {
+    function burn(uint256 _tokenId) external onlyRole(STAKING_CONTRACT_ROLE) {
       _burn(_tokenId);
 
       emit Burned(_tokenId);
     }
   
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
       internal
       virtual
       override(ERC721)
     {
       if(from != address(0) || to != address(0)) {
-        revert Souldbound();
+        revert SouldBound();
       }
-      super._beforeTokenTransfer(from, to, tokenId);
+      super._beforeTokenTransfer(from, to, tokenId, batchSize);
     }
 
     function _burn(uint256 tokenId) internal override(ERC721) {
@@ -60,5 +60,16 @@ contract SoulBoundFoundersKey ERC721, Ownable, AccessControl {
 
     function setFoundersKeysAddress(address _foundersKeysAddress) external onlyRole(DEFAULT_ADMIN_ROLE) {
       foundersKeysAddress = _foundersKeysAddress;
+    }
+
+    function supportsInterface(bytes4 _interfaceId)
+      public
+      view
+      virtual
+      override(ERC721, AccessControl)
+      returns (bool)
+    {
+        return (ERC721.supportsInterface(_interfaceId) ||
+        AccessControl.supportsInterface(_interfaceId));
     }
 }
