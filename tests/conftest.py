b8d987ddc369dac4497d3e089091e6e1fef1277e
0xJax <jaxdoubleu@protonmail.com>
2023-01-08 15:46:49 +0100

    updated conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,33 +1,40 @@
 import pytest
 
-from brownie import SoulboundFoundersKey, accounts, ERC721AMock
+from brownie import (
+    SoulboundFoundersKey, 
+    accounts, 
+    ERC721AMock
+)
+from brownie.network.account import Account
 
 
 @pytest.fixture
-def admin():
+def admin() -> Account:
     return accounts[0]
 
 
 @pytest.fixture
-def from_admin(admin):
+def from_admin(admin) -> dict:
     return {'from': admin}
 
-@pytest.fixture
-def address():
-    return accounts[1]
 
 @pytest.fixture
-def from_address(address):
+def address() -> Account:
+    return accounts[1]
+
+
+@pytest.fixture
+def from_address(address) -> dict:
     return {'from': address}
 
 
 @pytest.fixture
-def deployed_erc721a_mock(from_admin):
+def deployed_erc721a_mock(from_admin) -> ERC721AMock:
     return ERC721AMock.deploy(from_admin)
 
 
 @pytest.fixture
-def deployed_soulbound(deployed_erc721a_mock, admin, from_admin):
+def deployed_soulbound(deployed_erc721a_mock, admin, from_admin) -> SoulboundFoundersKey:
     return SoulboundFoundersKey.deploy(
         admin.address,
         deployed_erc721a_mock.address,
