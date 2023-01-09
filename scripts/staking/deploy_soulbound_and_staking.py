ce11698a01e662537839c1c49fed94a6bc537c8c
0xJax <jaxdoubleu@protonmail.com>
2023-01-09 22:14:47 +0100

    fixed 100% tests

--- scripts/staking/deploy_soulbound_and_staking.py
+++ scripts/staking/deploy_soulbound_and_staking.py
@@ -1,5 +1,3 @@
-from brownie import accounts
-
 from ..tokens.deploy_soulbound_contract import deploy as deploy_soulbound
 from .deploy_staking_contract import deploy as deploy_staking
 
@@ -9,4 +7,5 @@ def deploy(private_key: str, founders_key_address: str):
     staking_address = deploy_staking(private_key, founders_key_address, soulbound_address)
     print(f"Founders Key Address: {founders_key_address}")
     print(f"Staking Address: {staking_address}")
+    return soulbound_address, staking_address
 
