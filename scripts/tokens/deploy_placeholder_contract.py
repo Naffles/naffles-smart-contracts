242606a26f2507159219127fa61ad6589dfeb996
Code Jax <110606352+c0dejax@users.noreply.github.com>
2023-01-10 16:25:30 +0100

    Update scripts/tokens/deploy_placeholder_contract.py
    Co-authored-by: root <115649481+r00tail@users.noreply.github.com>

--- scripts/tokens/deploy_placeholder_contract.py
+++ scripts/tokens/deploy_placeholder_contract.py
@@ -1,5 +1,6 @@
 from brownie import SoulboundFoundersKey, accounts, 
 
+
 def deploy(private_key: str, _foundersKeyAddress: str):
     account = accounts.add(private_key)
     SoulboundFoundersKey.deploy(account.address, _foundersKeyAddress, {'from': account})
