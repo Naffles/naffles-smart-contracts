d57aec1cb9c6907f24d45caff0c1939200732915
0xJax <jaxdoubleu@protonmail.com>
2023-01-14 12:00:20 +0100

    updated deploy scripts

--- scripts/staking/deploy_staking_contract.py
+++ scripts/staking/deploy_staking_contract.py
@@ -1,5 +1,6 @@
-from brownie import FoundersKeyStaking, ERC721AMock, accounts, SoulboundFoundersKey
+import json
 
+from brownie import FoundersKeyStaking, accounts, SoulboundFoundersKey, Contract
 
 def deploy(
     private_key: str, founders_key_address: str, soulbound_nft_address: str
@@ -9,8 +10,12 @@ def deploy(
         founders_key_address, soulbound_nft_address, {"from": account}, publish_source=True)
 
     address = deployed.address
-    print(f"Deployed to: {address}")
-    ERC721AMock.at(founders_key_address).setStakingAddress(address, {"from": account})
+
+    # open IfoundersKey json file 
+    with open("../../build/interfaces/IFoundersKey.json", "r") as f:
+        abi = json.load(f)["abi"]
+
+    Contract.from_abi("FoundersKey", founders_key_address, abi).setStakingAddress(address, {"from": account})
     soulbound_contract = SoulboundFoundersKey.at(soulbound_nft_address)
     soulbound_contract.grantRole(
         soulbound_contract.STAKING_CONTRACT_ROLE(), address, {"from": account}
