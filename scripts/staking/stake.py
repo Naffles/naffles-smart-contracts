57689ab38c0d53e9bfe4f7e839a05b28d79a84ed
0xJax <jaxdoubleu@protonmail.com>
2023-01-17 10:45:53 +0100

    removed stake script

--- scripts/staking/stake.py
+++
@@ -1,6 +0,0 @@
-from brownie import FoundersKeyStaking, accounts
-
-def stake(private_key: str, staking_contract: str): 
-    account = accounts.add(private_key)
-    FoundersKeyStaking.at(staking_contract).stake(51, 1, {'from': account})
-
