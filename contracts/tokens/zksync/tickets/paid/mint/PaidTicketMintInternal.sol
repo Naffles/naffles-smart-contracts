cabe03b259d3ab00dd2e9f4bab4842448b0eb2a1
0xJax <jaxdoubleu@protonmail.com>
2023-02-13 21:15:06 +0100

    extend correct interface

--- contracts/tokens/zksync/tickets/paid/mint/PaidTicketMintInternal.sol
+++ contracts/tokens/zksync/tickets/paid/mint/PaidTicketMintInternal.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.17;
 import { PaidTicketBaseStorage } from "./PaidTicketBaseStorage.sol";
 import { IPaidTicketMintInternal } from "../../../../../../interfaces/tokens/zksync/tickets/paid/mint/IPaidTicketMintInternal.sol";
 
-contract PaidTicketMintInternal is {
+contract PaidTicketMintInternal is IPaidTicketMintInternal {
   function buyTickets(uint256 _amount, address naffle) internal {
      
   }
