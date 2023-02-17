3af5bd0d98fae7bd300b0fe17a7c0d3e08e9b280
0xJax <jaxdoubleu@protonmail.com>
2023-02-17 08:57:37 +0100

    added createNaffle to NaffleHolderBase

--- contracts/naffle/ethereum/naffle/base/NaffleHolderBase.sol
+++ contracts/naffle/ethereum/naffle/base/NaffleHolderBase.sol
@@ -4,10 +4,30 @@ pragma solidity ^0.8.17;
 import {IERC721Receiver} from "@solidstate/contracts/interfaces/IERC721Receiver.sol";
 import {IERC1155Receiver} from "@solidstate/contracts/interfaces/IERC1155Receiver.sol";
 import {NaffleHolderBaseInternal} from "./NaffleHolderBaseInternal.sol";
+import { NaffleTypes } from '../../../../libraries/NaffleTypes.sol';
 
 error NotSupported();
 
 abstract contract NaffleHolderBase is NaffleHolderBaseInternal, IERC721Receiver, IERC1155Receiver {
+  function createNaffle(
+      address _ethTokenAddress, 
+      address _owner,
+      uint256 _nftId, 
+      uint256 _paidTicketSpots,
+      uint256 _ticketPriceInWei,
+      uint256 _endTime, 
+      NaffleTypes.NaffleType _naffleType
+  ) internal returns (uint256 naffleId) {
+      return _createNaffle(
+          _ethTokenAddress, 
+          _owner,
+          _nftId, 
+          _paidTicketSpots,
+          _endTime, 
+          _ticketPriceInWei,
+          _naffleType
+      );
+  }
   function onERC721Received(
     address,
     address,
@@ -46,4 +66,5 @@ abstract contract NaffleHolderBase is NaffleHolderBaseInternal, IERC721Receiver,
   function supportsInterface(bytes4 interfaceId) external pure override returns (bool) {
     return interfaceId == type(IERC721Receiver).interfaceId || interfaceId == type(IERC1155Receiver).interfaceId;
   }
+
 }
