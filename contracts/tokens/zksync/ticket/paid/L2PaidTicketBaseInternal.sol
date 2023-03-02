23f45ef35a98b9f4a30af8e7b8112fb0b659e661
0xJax <jaxdoubleu@protonmail.com>
2023-03-02 16:11:24 +0100

    save gass

--- contracts/tokens/zksync/ticket/paid/L2PaidTicketBaseInternal.sol
+++ contracts/tokens/zksync/ticket/paid/L2PaidTicketBaseInternal.sol
@@ -15,12 +15,11 @@ import "../../../../../interfaces/naffle/zksync/IL2NaffleView.sol";
 abstract contract L2PaidTicketBaseInternal is IL2PaidTicketBaseInternal, AccessControlInternal, ERC721BaseInternal, ERC721EnumerableInternal {
     event Log(string message, uint data);
 
-    function _mintTickets(address _to, uint256 _amount, uint256 _naffleId, uint256 _ticketPriceInWei) internal returns(uint256[] memory) {
+    function _mintTickets(address _to, uint256 _amount, uint256 _naffleId, uint256 _ticketPriceInWei, uint256 startingTicketId) internal returns(uint256[] memory) {
         L2PaidTicketStorage.Layout storage l = L2PaidTicketStorage.layout();
-        NaffleTypes.L2Naffle memory naffle = IL2NaffleView(l.l2NaffleContractAddress).getNaffleById(_naffleId);
         uint256[] memory ticketIds = new uint256[](_amount);
         uint256 count = 0;
-        for (uint256 i = naffle.numberOfPaidTickets - _amount; i < naffle.numberOfPaidTickets; ++i) {
+        for (uint256 i = startingTicketId; i < startingTicketId + _amount; ++i) {
             uint256 ticketId = i + 1;
             NaffleTypes.PaidTicket
                 memory paidTicket = NaffleTypes.PaidTicket({
