9abc33c923bc4f1252ddfa3bfd9c61b8075f1959
root <r00tail@protonmail.com>
2023-03-15 09:54:44 +0100

    Publish source of proxy

--- scripts/staking/deploy_staking_contract.py
+++ scripts/staking/deploy_staking_contract.py
@@ -1,19 +1,27 @@
 from brownie import (Contract, ERC1967Proxy, FoundersKeyStaking,
-                     SoulboundFoundersKey, interface)
+                     SoulboundFoundersKey, accounts, interface)
+from brownie.network.account import Account
 
 
 def deploy(
-    account=None,
+    account: Account | None = None,
     founders_key_address: str | None = None,
     soulbound_nft_address: str | None = None,
     publish_source: bool = True,
 ) -> str:
+    if not account:
+        account = accounts.load("naffles")
     deployed = FoundersKeyStaking.deploy(
         {"from": account},
         publish_source=publish_source,
     )
 
-    proxy = ERC1967Proxy.deploy(deployed.address, b"", {"from": account})
+    proxy = ERC1967Proxy.deploy(
+        deployed.address,
+        b"",
+        {"from": account},
+        publish_source=publish_source,
+    )
     proxy = Contract.from_abi("FoundersKeyStaking", proxy.address, deployed.abi)
     proxy.initialize({"from": account})
 
