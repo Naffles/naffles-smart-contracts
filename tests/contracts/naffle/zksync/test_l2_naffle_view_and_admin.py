5c7b2ad255678148b7900eef70c8a45ace7cc745
Rik van der Werf <rikvanderwerf@riks-mac-mini.lan>
2023-03-23 16:31:34 +0100

    fixed admin tests

--- tests/contracts/naffle/zksync/test_l2_naffle_view_and_admin.py
+++ tests/contracts/naffle/zksync/test_l2_naffle_view_and_admin.py
@@ -469,3 +469,96 @@ def test_cancel_naffle_invalid_naffle_id(
 
     with brownie.reverts(get_error_message("InvalidNaffleId", ['uint256'], [2])):
         admin_facet.adminCancelNaffle(2, from_admin)
+
+
+def test_cancel_naffle_invalid_status(
+    address,
+    from_address,
+    from_admin,
+    deployed_l2_naffle_diamond,
+    deployed_l2_naffle_base_facet,
+    deployed_l2_naffle_admin_facet,
+    deployed_l2_naffle_view_facet,
+    deployed_founders_key_staking,
+    deployed_erc721a_mock,
+    deployed_eth_zksync_mock,
+    deployed_l1_messenger_mock
+):
+    (
+        access_control,
+        base_facet,
+        admin_facet,
+        view_facet,
+    ) = setup_l2_naffle_diamond_with_facets(
+        from_admin,
+        deployed_l2_naffle_diamond,
+        deployed_l2_naffle_base_facet,
+        deployed_l2_naffle_admin_facet,
+        deployed_l2_naffle_view_facet,
+    )
+    setup_l2_naffle_contract(
+        admin_facet,
+        from_admin["from"],
+        from_admin["from"],
+        from_admin["from"],
+        deployed_l1_messenger_mock,
+        from_admin,
+    )
+    endtime = datetime.datetime.now().timestamp() + 1000
+
+    base_facet.createNaffle(
+        (
+            deployed_erc721a_mock.address,
+            address,
+            NAFFLE_ID,
+            NFT_ID,
+            PAID_TICKET_SPOTS,
+            TICKET_PRICE,
+            endtime,
+            STANDARD_NAFFLE_TYPE,
+            ERC721,
+        ),
+        from_admin,
+    )
+
+    admin_facet.adminCancelNaffle(NAFFLE_ID, from_admin)
+    with brownie.reverts(get_error_message("InvalidNaffleStatus", ['uint8'], [2])):
+        admin_facet.adminCancelNaffle(NAFFLE_ID, from_admin)
+
+
+def test_cancel_naffle_not_allowed(
+    address,
+    from_address,
+    from_admin,
+    deployed_l2_naffle_diamond,
+    deployed_l2_naffle_base_facet,
+    deployed_l2_naffle_admin_facet,
+    deployed_l2_naffle_view_facet,
+    deployed_founders_key_staking,
+    deployed_erc721a_mock,
+    deployed_eth_zksync_mock,
+    deployed_l1_messenger_mock
+):
+    (
+        access_control,
+        base_facet,
+        admin_facet,
+        view_facet,
+    ) = setup_l2_naffle_diamond_with_facets(
+        from_admin,
+        deployed_l2_naffle_diamond,
+        deployed_l2_naffle_base_facet,
+        deployed_l2_naffle_admin_facet,
+        deployed_l2_naffle_view_facet,
+    )
+    setup_l2_naffle_contract(
+        admin_facet,
+        from_admin["from"],
+        from_admin["from"],
+        from_admin["from"],
+        deployed_l1_messenger_mock,
+        from_admin,
+    )
+
+    with brownie.reverts():
+        admin_facet.adminCancelNaffle(NAFFLE_ID, from_address)
