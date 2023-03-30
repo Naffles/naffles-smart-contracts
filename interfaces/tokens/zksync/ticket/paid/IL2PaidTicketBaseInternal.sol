7fa565dc77b2d89278106df32e2e4848115ea68c
Jax <jaxdoubleu@protonmail.com>
2023-03-30 16:19:38 +0200

    added paid ticket comments

--- interfaces/tokens/zksync/ticket/paid/IL2PaidTicketBaseInternal.sol
+++ interfaces/tokens/zksync/ticket/paid/IL2PaidTicketBaseInternal.sol
@@ -3,10 +3,35 @@ pragma solidity ^0.8.17;
 
 import "../../../../../contracts/libraries/NaffleTypes.sol";
 
+/**
+ * @title interface for L2PaidTicketBaseInternal
+ */
 interface IL2PaidTicketBaseInternal {
+    /**
+     * @notice thrown when the msg.sender is not allowed to call the method.
+     */
     error NotAllowed();
+
+    /**
+     * @notice thrown when the naffle is not cancelled
+     * @param status the current status of the naffle.
+     */
     error NaffleNotCancelled(NaffleTypes.NaffleStatus status);
+
+    /**
+     * @notice thrown when an invalid ticket id is passed.
+     * @param ticketId the ticket id passed.
+     */
     error InvalidTicketId(uint256 ticketId);
+
+    /**
+     * @notice thrown when the refund transaction failed.
+     */
     error RefundFailed();
+
+    /**
+     * @notice thrown when the sender is not the owner of the ticket.
+     * @param sender the called of the method.
+     */
     error NotTicketOwner(address sender);
 }
