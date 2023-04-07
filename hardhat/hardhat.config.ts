f91448bd1026121abd7fc7d50278d193155f566e
Jax <jaxdoubleu@protonmail.com>
2023-04-07 14:23:57 +0200

    stash l2 deployment

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -13,19 +13,24 @@ console.log("INFURA_API_KEY", INFURA_API_KEY)
 
 module.exports = {
   zksolc: {
-    version: "1.3.5",
+    version: "1.3.8",
     compilerSource: "binary",
     settings: {},
   },
   defaultNetwork: "zkSyncTestnet",
 
   networks: {
+    localhost: {
+      zksync: true,  // enables zksync in hardhat local network
+      ethNetwork: "goerli",
+      url: "http://localhost:8545"
+    },
     goerli: {
-        url: "https://goerli.infura.io/v3/<API_KEY>" // URL of the Ethereum Web3 RPC (optional)
+        url: "https://goerli.infura.io/v3/" + INFURA_API_KEY // URL of the Ethereum Web3 RPC (optional)
     },
     zkSyncTestnet: {
       url: "https://zksync2-testnet.zksync.dev",
-      ethNetwork: "goerli", // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+      ethNetwork: "goerli",
       zksync: true,
     },
   },
