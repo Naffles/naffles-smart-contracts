1810bc1247a1c03c7bb5f6189300c8e33ea4b60f
Jax <jaxdoubleu@protonmail.com>
2023-04-07 14:37:54 +0200

    reset

--- hardhat/contracts/example/ITestValueFacet.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.17;
-
-interface ITestValueFacet {
-    function getValue() external view returns (string memory);
-    function setValue(string memory value) external;
-}
