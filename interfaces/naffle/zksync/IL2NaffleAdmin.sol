e9935c7807683e62a73806ea43c1fa2055468eac
Jax <jaxdoubleu@protonmail.com>
2023-04-12 11:47:25 +0200

    updated pr

--- interfaces/naffle/zksync/IL2NaffleAdmin.sol
+++ interfaces/naffle/zksync/IL2NaffleAdmin.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.17;
 
 interface IL2NaffleAdmin {
     function setPlatformFee(uint256 _platformFee) external;
-    function setOpenEntryRatio(uint256 _freeTicketRatio) external;
+    function setOpenEntryRatio(uint256 _openEntryTicketRatio) external;
     function setAdmin(address _admin) external;
     function setL1NaffleContractAddress(address _l1NaffleContractAddress) external;
     function setPaidTicketContractAddress(address _paidTicketContractAddress) external;
