ae5936f529f441a71939d0c9848d2518b707cf80
Jax <jaxdoubleu@protonmail.com>
2023-04-12 05:05:39 +0200

    added constructors

--- hardhat/deploy/deploy_l2_naffle_diamond.ts
+++ hardhat/deploy/deploy_l2_naffle_diamond.ts
@@ -1,22 +1,25 @@
 import { config as dotEnvConfig } from "dotenv";
 dotEnvConfig();
 
-import { utils, Wallet } from "zksync-web3";
+import { utils, Wallet, Provider} from "zksync-web3";
 import * as ethers from "ethers";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
 
-export default async function (hre: HardhatRuntimeEnvironment) {
-  // Initialize the wallet.
-  const wallet = new Wallet(process.env.PRIVATE_KEY);
 
+export default async function (hre: HardhatRuntimeEnvironment) {
+
+  // Initialize the wallet.
+  const provider = Provider.getDefaultProvider();
+  //   const wallet = new Wallet(process.env.PRIVATE_KEY);
+  const wallet = new Wallet("0x7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38edeab82d4110");
+
+  console.log("NEW WALLET", provider)
   // Create deployer object and load the artifact of the contract we want to deploy.
   const deployer = new Deployer(hre, wallet);
   const artifact = await deployer.loadArtifact("L2NaffleDiamond");
-  console.log(artifact)
 
-
-  const l2NaffleDiamond = await deployer.deploy(artifact)
+  const l2NaffleDiamond = await deployer.deploy(artifact, [wallet.address])
 
   // Show the contract info.
   const contractAddress = l2NaffleDiamond.address;
--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -11,6 +11,19 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY
 
 console.log("INFURA_API_KEY", INFURA_API_KEY)
 
+const zkSyncTestnet =
+  process.env.NODE_ENV == "test"
+    ? {
+        url: "http://localhost:3050",
+        ethNetwork: "http://localhost:8545",
+        zksync: true,
+      }
+    : {
+        url: "https://testnet.era.zksync.dev",
+        ethNetwork: "goerli",
+        zksync: true,
+      };
+
 module.exports = {
   zksolc: {
     version: "1.3.8",
@@ -18,21 +31,19 @@ module.exports = {
     settings: {},
   },
   defaultNetwork: "zkSyncTestnet",
-
   networks: {
-    localhost: {
-      zksync: true,  // enables zksync in hardhat local network
-      ethNetwork: "goerli",
-      url: "http://localhost:8545"
+    hardhat: {
+      zksync: true,
+    },
+    testnet: {
+        url: "http://localhost:3050",
+        ethNetwork: "hardhat",
+        zksync: true,
     },
     goerli: {
         url: "https://goerli.infura.io/v3/" + INFURA_API_KEY // URL of the Ethereum Web3 RPC (optional)
     },
-    zkSyncTestnet: {
-      url: "https://zksync2-testnet.zksync.dev",
-      ethNetwork: "goerli",
-      zksync: true,
-    },
+    zkSyncTestnet,
   },
   solidity: {
     version: "0.8.17",
