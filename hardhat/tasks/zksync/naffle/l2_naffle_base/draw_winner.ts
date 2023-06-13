4d7509a86c17ba0e988b250a80ed3fc622b4617d
codejax <jaxdoubleu@protonmail.com>
2023-06-13 15:42:47 +0200

    correctly drawing winner

--- hardhat/tasks/zksync/naffle/l2_naffle_base/draw_winner.ts
+++ hardhat/tasks/zksync/naffle/l2_naffle_base/draw_winner.ts
@@ -7,18 +7,24 @@ task("draw-winner", "Draws the winner for a naffle")
   .addParam("l2nafflecontractaddress", "The address of the Naffle Diamond contract deployed on the zkSync Layer 2 (L2) network.")
   .addParam("naffleid", "The unique identifier of the Naffle event for which draw the winner.")
   .setAction(async (taskArgs, hre) => {
-      const l2provider = new Provider(getRPCEndpoint(hre.network.name));
-      const walletL2 = new Wallet(getPrivateKey(), l2provider);
+    const l2provider = new Provider(getRPCEndpoint(hre.network.name));
+    const walletL2 = new Wallet(getPrivateKey(), l2provider);
 
-      const contractBaseFactory = await hre.ethers.getContractFactory("L2NaffleBase");
-      const l2ContractInstance = contractBaseFactory.attach(taskArgs.l2nafflecontractaddress);
+    const contractBaseFactory = await hre.ethers.getContractFactory("L2NaffleBase");
+    const l2ContractInstance = contractBaseFactory.attach(taskArgs.l2nafflecontractaddress);
 
-      console.log("drawing winner..")
-      const drawWinnerTransaction= await l2ContractInstance.connect(walletL2).drawWinner(
-        parseInt(taskArgs.naffleid)
-      )
+    const contractViewFactory = await hre.ethers.getContractFactory("L2NaffleView");
+    const l2ContractViewInstance = contractViewFactory.attach(taskArgs.l2nafflecontractaddress);
 
-      const winnerTxReceipt = await drawWinnerTransaction.wait()
-      console.log(winnerTxReceipt)
-    }
-  );
+    console.log("Getting naffle info..")
+    const naffle = await l2ContractViewInstance.connect(walletL2).getNaffleById(hre.ethers.BigNumber.from(taskArgs.naffleid))
+    console.log(naffle)
+
+    console.log("drawing winner..")
+    const drawWinnerTransaction= await l2ContractInstance.connect(walletL2).drawWinner(
+      parseInt(taskArgs.naffleid)
+    )
+
+    const winnerTxReceipt = await drawWinnerTransaction.wait()
+    console.log(winnerTxReceipt)
+  });
