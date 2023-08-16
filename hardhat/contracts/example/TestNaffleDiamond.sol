8c536eab94556dbf60426c5c37a6aa0a3bd009c5
Rik van der Werf <rikvanderwerf@coderstudio.nl>
2023-08-16 12:26:57 +0200

    added rnadomNumberRequested to L2Naffle and updated tests

--- hardhat/contracts/example/TestNaffleDiamond.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.17;
-
-import "@solidstate/contracts/proxy/diamond/SolidStateDiamond.sol";
-
-contract TestNaffleDiamond is SolidStateDiamond {}
