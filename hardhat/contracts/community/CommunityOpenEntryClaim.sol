ba6196774936a1f7139c096045007de440871ebf
curi0n-s <81062335+curi0n-s@users.noreply.github.com>
2023-09-25 06:46:37 -0400

    I remembered how signatures work now lol

--- hardhat/contracts/community/CommunityOpenEntryClaim.sol
+++ hardhat/contracts/community/CommunityOpenEntryClaim.sol
@@ -5,12 +5,15 @@ pragma solidity 0.8.19;
 /// @notice this contract uses signatures to allow users from selected communities to claim open entry tickets
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import { SignatureChecker } from "../libraries/SignatureChecker.sol";
+import { SignatureChecker } from "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
+import { ECDSA } from "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
 import { IL2OpenEntryTicketAdmin } from "../interfaces/IL2OpenEntryTicketAdmin.sol";
 
 contract CommunityOpenEntryClaim is Ownable {
 
     IL2OpenEntryTicketAdmin public openEntryTicketAdmin;
+
+    address private signer;
 
     mapping(address => uint256) public claimedTickets;
 
@@ -19,9 +22,14 @@ contract CommunityOpenEntryClaim is Ownable {
 
     /**
      * @notice set the address of the L2OpenEntryTicketAdmin contract which calls adminMint
+     * and set the signer address for signatures
      */
-    constructor(address _openEntryTicketAdmin) {
+    constructor(
+        address _openEntryTicketAdmin,
+        address _signer
+    ) {
         openEntryTicketAdmin = IL2OpenEntryTicketAdmin(_openEntryTicketAdmin);
+        signer = _signer;
     }
 
     /**
@@ -34,23 +42,30 @@ contract CommunityOpenEntryClaim is Ownable {
         uint256 _maxClaimable,
         bytes _signature
     ) external {
-            bool isValidSignature = SignatureChecker.isValidSignatureNow(
-                _recipient,
-                _amount,
-                _maxClaimable,
-                _signature,
-                block.chainid
+        bool isValidSignature =
+            SignatureChecker.isValidSignatureNow(
+                signer,
+                ECDSA.toEthSignedMessageHash(
+                    keccak256(
+                        abi.encodePacked(
+                            _recipient,
+                            _maxClaimable
+                            block.chainid
+                        )
+                    )
+                ),
+                _signature
             );
 
-            if(!isValidSignature){
-                revert InvalidSignature();
-            }
+        if(!isValidSignature){
+            revert InvalidSignature();
+        }
 
-            if(claimedTickets[_recipient] + _amount > _maxClaimable){
-                revert TransactionWillExceedClaimMax();
-            }
+        if(claimedTickets[_recipient] + _amount > _maxClaimable){
+            revert TransactionWillExceedClaimMax();
+        }
 
-            openEntryTicketAdmin.adminMint(_recipient, _amount);
+        openEntryTicketAdmin.adminMint(_recipient, _amount);
     }
 
 }
